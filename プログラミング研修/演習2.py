20cbac3703643c7c96f07a041280f603cff5d18b
Rinka.Sakita <rinkasakita@gmail.com>
2022-04-20 16:56:51 +0900

    演習１～７

---
+++ プログラミング研修/演習2.py
@@ -0,0 +1,10 @@
+# 3と5, 3, 5で割り切れるかの場合分け
+for i in range(1, 31):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
